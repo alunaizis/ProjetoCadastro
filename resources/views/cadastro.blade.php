3bf7969f589cbda7d9dc5127a79c38da3734ca09
alunaizis <izisp@acad.ifms.edu.br>
2021-03-25 10:20:37 -0300

    formulario

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -15,49 +15,29 @@
 </style>
 </head>
 <body>
-<form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4">
+<div class="form-group row">
+  <label for="nomecompleto" class="col-2 col-form-label">Nome Completo</label>
+  <div class="col-10">
+    <input class="form-control" type="text"  id="nomecompleto">
   </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4">
+</div>
+<div class="form-group row">
+  <label for="senha" class="col-2 col-form-label">Senha</label>
+  <div class="col-10">
+    <input class="form-control" type="password"  id="senha">
   </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+</div>
+<div class="form-group row">
+  <label for="matricula" class="col-2 col-form-label">Matricula</label>
+  <div class="col-10">
+    <input class="form-control" type="number"  id="matricula">
   </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+</div>
+<div class="form-group row">
+  <label for="datadenascimento" class="col-2 col-form-label">Data de Nascimento</label>
+  <div class="col-10">
+    <input class="form-control" type="date" id="datadenascimento">
   </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity">
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip">
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
+</div>
 </body>
 </html>
